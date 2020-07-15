7c613328bb3ab0a1a99c462f92936ce7c9302b33
Hassan Fayaz <fayax555@gmail.com>
2020-07-16 00:27:41 +0500

    added countdown timer

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 const btn1 = document.querySelector('#btn1')
-btn1.classList.add('active')
+const parallax = document.querySelector('.parallax');
 
+// parallax scroll effect
+window.addEventListener('scroll', e =>
+	parallax.style.backgroundPositionY = window.pageYOffset * 0.8 + 'px'
+)
+
+// tabs to change schedule date
+btn1.classList.add('active')
 tabs.forEach(tab => {
 	tab.addEventListener('click', e => {
 		const target = document.querySelector(tab.dataset.tabTarget)
@@ -19,5 +26,31 @@ tabs.forEach(tab => {
 	})
 })
 
+// Countdown Timer
+// targetDate is 20 Dec 2020 at 00:00
+const targetDate = new Date(2020, 12, 20, 9, 0, 0, 0);
+
+const $day = document.querySelector('.day'),
+	$hour = document.querySelector('.hour'),
+	$min = document.querySelector('.minute'),
+	$sec = document.querySelector('.second');
+
+const format = (num) => {
+	str = num.toString(); // convert number to string
+	return str.length > 1 ? str : '0' + str;
+}
+
+setInterval(() => {
+	const remainingTime = targetDate - new Date(),
+		dayRemaing = Math.floor(remainingTime / 1000 / 60 / 60 / 24),
+		hourRemaing = Math.floor(remainingTime / 1000 / 60 / 60) % 24,
+		minRemaing = Math.floor(remainingTime / 1000 / 60) % 60,
+		secRemaing = Math.floor(remainingTime / 1000) % 60;
+
+	$day.textContent = format(dayRemaing)
+	$hour.textContent = format(hourRemaing)
+	$min.textContent = format(minRemaing)
+	$sec.textContent = format(secRemaing)
+}, 10);
 
 
